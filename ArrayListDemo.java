4cb005a6c05e4a231119ce6c956bc6b090b5bf3a
Chenjamin <chenjamin777@gmail.com>
2018-11-27 10:19:30 -0500

    Added print method
    Added a method that prints out the ArrayList

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -2,10 +2,17 @@ import java.util.ArrayList;
 
 public class ArrayListDemo{
 
-	public static ArrayList<Integer> combine(ArrayList <Integer> a, ArrayList <Integer> b){
+	public static ArrayList<Integer> combine(ArrayList<Integer> a, ArrayList<Integer> b){
 		for(int i = 0; i < b.size(); i++){
 			a.add(b.get(i));
 		}
 		return a;
+	}
+
+	public static void print(ArrayList<Integer> a){
+		for(int i = 0; i < a.size(); i++){
+			System.out.print(a.get(i) + ", ");
+		}
+		System.out.println("");
 	}
 }
